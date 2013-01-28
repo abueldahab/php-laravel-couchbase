9789cd309cd8c72f7d4a9505bd31d17bf64a8529
scalabl3 <jasdeep@scalabl3.com>
2013-01-28 12:54:30 -0800

    Update couchbase_connect.php

--- couchbase_connect.php
+++ couchbase_connect.php
@@ -12,19 +12,21 @@ class CouchbaseConnect {
   {
   
     // retrieve settings from laravel-root/config/database.php 
-		extract(Config::get('database.connections.couchbase'));
+    extract(Config::get('database.connections.couchbase'));
 
     
     // create connection and save into static $cb class var
-	  CouchbaseConnect::$cb = new Couchbase($host, $bucket, $password, $bucket, true);
-
-		return CouchbaseConnect::$cb;
-	}
+    CouchbaseConnect::$cb = new Couchbase($host, $bucket, $password, $bucket, true);
+    
+    return CouchbaseConnect::$cb;
+  }
  
 
   // can be used to get, or CouchbaseConnect::$cb, but if connection hasn't been made, make it
-  public static function connection() {
+  public static function connection() 
+  {
     
+    // if static variable isn't defined or is null, try to make the connection again
     if (!isset(CouchbaseConnect::$cb) || CouchbaseConnect::$cb == NULL) {
       return CouchbaseConnect::connect();
     }
